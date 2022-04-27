da243c6da2017c0348d760e1760ca68d79b22093
AlysteBlackGit <vinhhiepbn2003.work@gmail.com>
2022-04-27 15:04:46 +0700

    Update Combinstions-Generator.cpp

--- Generate-Algo/Combinstions-Generator.cpp
+++ Generate-Algo/Combinstions-Generator.cpp
@@ -8,23 +8,15 @@ void print(vector<int> a){
 void combinations_generator(int n){
 	vector<int> a(n,0);
 	print(a);
-	a[a.size()-1]=1;
-	print(a);
-	int i=n;
+	ll i=n-1;
 	while(i>=0){
-		--i;
-		if(i>=0&&a[i]==0&&a[i+1]==1){
-			a[i]=1;
-			for(int j=i+1;j<n;++j)
-				a[j]=0;
-			print(a);
-			i=n;
-		}
-		else if(i>=0&&a[i]==1&&a[i+1]==0){
-			a[a.size()-1]=1;
-			i=n;
+		if(!a[i]){
+			++a[i];
+			FOR(j,i+1,n) a[j]=0;
+			i=n-1;
 			print(a);
 		}
+		else --i;
 	}
 }
 int main(){
