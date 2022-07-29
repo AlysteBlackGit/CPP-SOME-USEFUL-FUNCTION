d5978496c57a8a3308e5719e198f078b6272f728
Allyste <vinhhiepbn2003.work@gmail.com>
2022-07-29 23:05:51 +0700

    Updated

--- Heading-Note.cpp
+++ Heading-Note.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-							/* DEFINE AREA */
+                            /* DEFINE AREA */
 
  /* Definition */                                           /* Meaning */
 
@@ -39,6 +39,20 @@ using namespace std;
 #define umsll                                   unordered_multiset<ll>
 #define umsc                                    unordered_multiset<ch>
 #define umsstr                                  unordered_multiset<str>
+#define mllll									map<ll,ll>
+#define mllstr 									map<ll,str>
+#define mstrll									map<str,ll>
+#define mcll									map<ch,ll>
+#define mllc									map<ll,ch>
+#define mcstr									map<ch,str>
+#define mstrc									map<str,ch>
+#define umllll									unordered_map<ll,ll>
+#define umllstr									unordered_map<ll,str>
+#define umstrll									unordered_map<str,ll>
+#define umcll									unordered_map<ch,ll>
+#define umllc									unordered_map<ll,ch>
+#define umcstr									unordered_map<ch,str>
+#define umstrc									unordered_map<str,ch>
 #define qll 									queue<ll>
 #define qstr									queue<str>
 #define qc										queue<ch>
@@ -47,95 +61,98 @@ using namespace std;
 #define pb                                      push_back
 #define pob                                     pop_back
 #define rev                                     reverse
-#define bs                                      binary_search
 #define rs                                      resize
 #define ins                                     insert
 #define era                                     erase
+#define cl										clear
 #define mp                                      make_pair
 #define fr										front
 #define f                                       first
 #define s                                       second
+#define bs                                      binary_search
+#define lb										lower_bound
+#define ub										upper_bound
 #define Test()                                  ll t; cin >> t; while (t--)
 const ll mod_val =                              1e9 + 7;
 
-							/* INLINE FUNCTIONS */
+                            /* INLINE FUNCTIONS */
 
-		//  Code  //                                                                                   //   Note  //
+        //  Code  //                                                                                   //   Note  //
 
 inline void swap_xor(ll &a, ll &b){
-	a^=b;                                                                                   
-	b^=a;                                                                                   /* ANOTHER WAY FOR SWAP 2 VALUES */
-	a^=b;
+    a^=b;                                                                                   
+    b^=a;                                                                                   /* ANOTHER WAY FOR SWAP 2 VALUES */
+    a^=b;
 }
 
 inline void split_string(str a, vs &res){
-	stringstream ss;
-	ss<<a;
-	str temp;
-	while(ss>>temp){                                                                        /* SPLITING WORDS IN SENTENCE HAS WHITE SPACES BETWEEN THEM */
-		res.pb(temp);
-		temp="";
-	}
+    stringstream ss;
+    ss<<a;
+    str temp;
+    while(ss>>temp){                                                                        /* SPLITING WORDS IN SENTENCE HAS WHITE SPACES BETWEEN THEM */
+        res.pb(temp);
+        temp="";
+    }
 }
 
 inline ll random(ll a, ll b){
-	return (a+(ll)(rand()*(b-a+1.0)/(1.0+RAND_MAX)));                                       /* GENERATING RANDOM VALUE, USING FOR MILLER-RABIN PRIME TEST */
+    return (a+(ll)(rand()*(b-a+1.0)/(1.0+RAND_MAX)));                                       /* GENERATING RANDOM VALUE, USING FOR MILLER-RABIN PRIME TEST */
 }
 
 inline bool miller_prime(ll n){
-	if(n<2 || (!(n%2)&&n>2)) return false;
-	bool check=true;
-	db output_value;                                                                       
-	ll input_value=log2(n-1);                                                               /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH NUMBERS FROM 0 TO 100 */
-	while(input_value>0){
-		output_value=(n-1)/pow(2,input_value);
-		if((ll)output_value==output_value && (ll)output_value%2) break;
-		--input_value;
-	}                                                                                       
-	ll ans=0;
-	loop(i,1,4){
-		ll random_value=random(2,n-2);
-		ans=(ll)pow(random_value,output_value)%n;
-		if(ans!=1 && ans!=n-1){
-			check=false;
-			break;	
-		}
-	}
-	if(check) return true;
-	ans=(ans*ans)%n;
-	if(ans==1 || ans==n-1) return true;
-	return false;
+    if(n<2 || (!(n%2)&&n>2)) return false;
+    bool check=true;
+    db output_value;                                                                       
+    ll input_value=log2(n-1);                                                               /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH NUMBERS FROM 0 TO 100 */
+    while(input_value>0){
+        output_value=(n-1)/pow(2,input_value);
+        if((ll)output_value==output_value && (ll)output_value%2) break;
+        --input_value;
+    }                                                                                       
+    ll ans=0;
+    loop(i,1,4){
+        ll random_value=random(2,n-2);
+        ans=(ll)pow(random_value,output_value)%n;
+        if(ans!=1 && ans!=n-1){
+            check=false;
+            break;	
+        }
+    }
+    if(check) return true;
+    ans=(ans*ans)%n;
+    if(ans==1 || ans==n-1) return true;
+    return false;
 }
 
 inline bool check_prime(ll n){
-	if(n<2 || (!(n%2)&&n>2)) return false;
-	loop_dis(i,3,sqrt(n)+1,2){                                                              /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH ALL NUMBERS */                                                        
-		if(!(n%i)) return false;                                                        
-	}                                                                                       /* TIME COMPLEXITY: O(SQRT(N)) */
-	return true;
+    if(n<2 || (!(n%2)&&n>2)) return false;
+    loop_dis(i,3,sqrt(n)+1,2){                                                              /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH ALL NUMBERS */                                                        
+        if(!(n%i)) return false;                                                        
+    }                                                                                       /* TIME COMPLEXITY: O(SQRT(N)) */
+    return true;
 }
 
 inline void build_Era_sieve(vll &sieve,ll size){
-	sieve[0]=sieve[1]=0;            
-	sieve[2]=1;                                                                                     
-	loop(i,2,sqrt(size)+1){                                                                 /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH ALL NUMBERS */
-		if(sieve[i]) loop_dis(j,i*i,size,i)                                                 
-			sieve[j]=0;                                                                     /* TIME COMPLEXITY: O(LOG(N)) */
-	}
+    sieve[0]=sieve[1]=0;            
+    sieve[2]=1;                                                                                     
+    loop(i,2,sqrt(size)+1){                                                                 /* FOR CHECKING WHETHER PRIME OR NOT PRIME WITH ALL NUMBERS */
+        if(sieve[i]) loop_dis(j,i*i,size,i)                                                 
+            sieve[j]=0;                                                                     /* TIME COMPLEXITY: O(LOG(N)) */
+    }
 }
 
 inline ll expo(ll n, ll m){
-	if(m==1) return n;                                                                      /* ANOTHER WAY FOR EVALUATING POW(N,M) */
-	if(!(m%2)) return expo(n,m/2) * expo(n,m/2);                                            
-	return expo(n,m/2) * expo(n,m/2) * n;                                                   /* TIME COMPLEXITY: O(LOG(N)) */
+    if(m==1) return n;                                                                      /* ANOTHER WAY FOR EVALUATING POW(N,M) */
+    if(!(m%2)) return expo(n,m/2) * expo(n,m/2);                                            
+    return expo(n,m/2) * expo(n,m/2) * n;                                                   /* TIME COMPLEXITY: O(LOG(N)) */
 }
-								/* INT MAIN */
+                                /* INT MAIN */
 int main(){
-	faster();
-	#ifndef ONLINE_JUDGE
-		freopen("input.txt","r",stdin);
-		freopen("output.txt","w",stdout);
-	#endif
-	Test(){
-	}
+    faster();
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    Test(){
+    }
 }
